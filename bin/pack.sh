355fcd86d934904f647722bc0ec1f37a79fcc9b1
alexandresalome <alexandre.salome@gmail.com>
2012-11-17 01:27:20 +0100

    script to pack download versions

---
+++ bin/pack.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+cd "$(dirname $0)"
+
+if [ "$1" == "" ]; then
+    echo "You must specify version to build"
+    exit 1
+fi
+
+if [ ! -d ../cache/pack ]; then
+    mkdir -p ../cache/pack
+fi
+
+cd ../cache/pack
+
+if [ ! -d repository ]; then
+    git clone https://github.com/gitonomy/gitonomy.git repository
+    cd repository
+else
+    cd repository
+    git fetch origin
+fi
+
+git checkout origin/$1
+./pack.sh
+
+cd ../../.. # back to project root
+
+if [ ! -d web/download ]; then
+    mkdir web/download
+fi
+
+mv cache/pack/repository/pack.tar.gz web/download/$1.tar.gz
